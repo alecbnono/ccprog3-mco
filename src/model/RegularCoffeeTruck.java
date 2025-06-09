f69b7f2d77f386e66274748f7d6070def9101db8
PrinceMPS <prinmat88@gmail.com>
2025-06-09 13:44:11 +0800

    Create constructor for RegularCoffeeTruck

--- src/model/RegularCoffeeTruck.java
+++ src/model/RegularCoffeeTruck.java
@@ -1,8 +1,19 @@
 package model;
 
+import java.util.Scanner;
+
 /**
  * RegularCoffeeTruck
  */
 public class RegularCoffeeTruck extends CoffeeTruck {
-
+    private int smallCup, mediumCup, largeCup;
+    private float coffeeBeans, milk, water;
+    public RegularCoffeeTruck() {
+        this.smallCup = 80;
+        this.mediumCup = 64;
+        this.largeCup = 40;
+        this.coffeeBeans = 1008;
+        this.milk = 640;
+        this.water = 640;
+    }
 }
