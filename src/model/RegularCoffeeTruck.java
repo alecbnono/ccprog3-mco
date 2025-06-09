f7bf53d9a857609414844e9a437b81193d080eee
PrinceMPS <prinmat88@gmail.com>
2025-06-09 13:54:30 +0800

    Add setter and getter for RegularCoffeeTruck

--- src/model/RegularCoffeeTruck.java
+++ src/model/RegularCoffeeTruck.java
@@ -16,4 +16,40 @@ public class RegularCoffeeTruck extends CoffeeTruck {
         this.milk = 640;
         this.water = 640;
     }
+    public int getsmallCup() {
+        return smallCup;
+    }
+    public int getmediumCup() {
+        return mediumCup;
+    }
+    public int getlargeCup() {
+        return largeCup;
+    }
+    public float getcoffeeBeans() {
+        return coffeeBeans;
+    }
+    public float getmilk() {
+        return milk;
+    }
+    public float getwater() {
+        return water;
+    }
+    public void setsmallCup(int smallCup) {
+        this.smallCup = smallCup;
+    }
+    public void setmediumCup(int mediumCup) {
+        this.mediumCup = mediumCup;
+    }
+    public void setlargeCup(int largeCup) {
+        this.largeCup = largeCup;
+    }
+    public void setcoffeeBeans(float coffeeBeans) {
+        this.coffeeBeans = coffeeBeans;
+    }
+    public void setmilk(float milk) {
+        this.milk = milk;
+    }
+    public void setwater(float water) {
+        this.water = water;
+    }
 }
