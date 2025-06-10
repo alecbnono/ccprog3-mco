e34b0b0b54db8c89ff2c6ff2d44da8e775d991e8
PrinceMPS <prinmat88@gmail.com>
2025-06-10 17:39:58 +0800

    Create MediumCup class

---
+++ src/model/MediumCup.java
@@ -0,0 +1,18 @@
+package model;
+
+public class MediumCup {
+        private final double VolumeinflOz = 12;
+        private int quantity;
+
+        public double getVolumeinflOz() {
+            return VolumeinflOz;
+        }
+        public int  getQuantity() {
+            return quantity;
+        }
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+
+
+}
