966f11c65c3379e47bb9f51a1d982d9c8846f0f3
PrinceMPS <prinmat88@gmail.com>
2025-06-10 17:49:33 +0800

    Create CoffeeBeans class

---
+++ src/model/CoffeeBeans.java
@@ -0,0 +1,11 @@
+package model;
+
+public class CoffeeBeans {
+    private double WeightInGrams;
+    public double getWeightInGrams() {
+        return WeightInGrams;
+    }
+    public void setWeightInGrams(double weightInGrams) {
+        this.WeightInGrams = weightInGrams;
+    }
+}
