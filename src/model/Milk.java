41840afc3ab2b74e5c8b3d7ddaabcb86ea6da0ce
PrinceMPS <prinmat88@gmail.com>
2025-06-10 18:11:12 +0800

    Create Milk class

---
+++ src/model/Milk.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Milk {
+    private double volumeinflOz;
+
+    public double getVolumeinflOz() {
+        return volumeinflOz;
+    }
+    public void setVolumeinflOz(double volumeinflOz) {
+        this.volumeinflOz = volumeinflOz;
+    }
+}
