03ce6a451c666c5010e13ed16dc396cabbf09260
PrinceMPS <prinmat88@gmail.com>
2025-06-14 21:15:33 +0800

    Modify StorageBox.java

--- src/model/StorageBox.java
+++ src/model/StorageBox.java
@@ -1,5 +1,21 @@
 package model;
 
 public class StorageBox {
-    
+    private Item storedItem;
+
+    public boolean emptyBox() {
+        return storedItem == null;
+    }
+    public boolean setBox(Item item) {
+        if (storedItem == null) {
+            storedItem = item;
+            return true;
+        }
+        else return false;
+    }
+
+    public Item getBox() {
+        return storedItem;
+    }
+
 }
