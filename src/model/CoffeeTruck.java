a8e017e40c96c2ccbe2c68a20b42bc1097056a79
PrinceMPS <prinmat88@gmail.com>
2025-06-15 12:36:15 +0800

    Create StorageBox.java

--- src/model/CoffeeTruck.java
+++ src/model/CoffeeTruck.java
@@ -4,5 +4,9 @@ package model;
  * CoffeeTruck
  */
 public class CoffeeTruck {
+    private StorageBox storageBox;
 
+    public StorageBox getStorageBox() {
+        return storageBox;
+    }
 }
--- src/model/StorageBox.java
+++ src/model/StorageBox.java
@@ -1,5 +1,9 @@
 package model;
 
+
 public class StorageBox {
-    
+    protected Item storedItem;
+    public StorageBox(Item storedItem) {
+        this.storedItem = storedItem;
+    }
 }
